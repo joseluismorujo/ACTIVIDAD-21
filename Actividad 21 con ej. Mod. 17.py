0e3e1f826138f783542394fa9a93f74c1cc23800
joseluismorujo <joseluismorujo@gmail.com>
2024-03-26 17:46:26 -0600

    Primera versión de la Actividad 21

---
+++ Actividad 21 con ej. Mod. 17.py
@@ -0,0 +1,16 @@
+# Importar un archivo CSV con Pandas
+
+# Se importa la librería pandas
+# La documentacion de la libreria se puede ver acá: https://pandas.pydata.org/docs/
+import pandas as pd
+
+# Otras librerias necesarias
+import numpy as np
+import os
+
+# Cambiar el directorio actual con chdir
+os.chdir('/users/Usuario/Desktop')
+
+# Se usaa la función read_csv para leer el archivo .csv
+# Este archivo es el de las películas de Netflix que hemos venido utilizando y lo ingesta en la variable df
+df = pd.read_csv('netflix_titles_2.csv')
